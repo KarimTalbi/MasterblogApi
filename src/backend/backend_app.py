5907410056237494416846d7a938f9b0b0ff9d81
Karim <karim.talbi7@gmail.com>
2025-11-19 20:38:42 +0100

    integrate Swagger UI for API documentation and add flask-swagger-ui dependency; update masterblog.json for detailed API schema

--- src/backend/backend_app.py
+++ src/backend/backend_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from data.json_handler import JsonHandler
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -54,17 +55,24 @@ def handle_post(post_id):
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
     posts = JsonHandler()
-
-    for arg in request.args:
-        if arg not in ['title', 'content', 'category', 'comment', 'tag']:
-            return {'error': f'Invalid search parameter: {arg}'}, 400
-
     search_result = posts.search(request.args)
-
     if search_result:
         return jsonify(search_result), 200
 
     return {'error': 'No posts found.'}, 404
+
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 
 if __name__ == '__main__':
