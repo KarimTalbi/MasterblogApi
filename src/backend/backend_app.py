f635534655b717bfe109784ad3fdcb0b3218f551
Karim <karim.talbi7@gmail.com>
2025-11-19 20:12:39 +0100

    refactor json_handler and backend_app for post management; add search, pagination, attachment handling, and enhanced JSON operations

--- src/backend/backend_app.py
+++ src/backend/backend_app.py
@@ -8,104 +8,56 @@ CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    posts = JsonHandler()
+
     if request.method == 'POST':
         data = request.get_json()
-        new_post = {
-            "id": unique_post_id(),
-            "title": data.get('title'),
-            "content": data.get('content')
-        }
-        if not new_post['title'] or not new_post['content']:
-            return {'error': f'Missing fields: {[key for key, value in new_post.items() if not value]}'}, 400
-        POSTS.append(new_post)
+
+        if not data['title'] or not data['content']:
+            return {'error': f'Missing fields: {[key for key, value in data.items() if not value]}'}, 400
+
+        new_post = posts.add(data)
         return jsonify(new_post), 201
 
-    else:
-        page = request.args.get('page', 1, type=int)
-        per_page = request.args.get('per_page', 10, type=int)
-
-        if per_page > 30:
-            per_page = 30
-
-        sort = request.args.get('sort', 'id')
-        direction = request.args.get('direction', 'asc')
-
-        sorted_posts = sorted(POSTS, key=lambda post: post.get(sort), reverse=(direction == 'desc'))
-        start = page * per_page - per_page
-        end = page * per_page
-        return jsonify(sorted_posts[start:end]), 200
+    return jsonify(posts.get(request.args)), 200
 
 
-@app.route('/api/auth/login', methods=['POST'])
-def login():
-    pass
-
-
-@app.route('/api/auth/register', methods=['POST'])
-def register():
-    pass
-
-
-@app.route('/api/posts/<int:post_id>', methods=['DELETE', 'PUT', 'POST'])
+@app.route('/api/posts/<int:post_id>', methods=['GET', 'DELETE', 'PUT', 'POST'])
 def handle_post(post_id):
-    global POSTS
+    posts = JsonHandler()
+
+    if request.method == 'GET':
+        post = posts.post(post_id)
+        if post:
+            return jsonify(post), 200
+
     if request.method == 'DELETE':
-        post_count = len(POSTS)
-        POSTS = [post for post in POSTS if post['id'] != post_id]
-        if len(POSTS) == post_count:
-            return {'error': f'Post with id <{post_id}> not found.'}, 404
-        return {'message': f'Post with id <{post_id}> has been deleted successfully.'}, 200
+        delete = posts.delete(post_id)
+        if delete:
+            return jsonify(delete), 200
 
-    elif request.method == 'PUT':
-        data = request.get_json()
-        for i, post in enumerate(POSTS):
-            if post['id'] == post_id:
-                for key in post:
-                    POSTS[i][key] = data.get(key) if data.get(key) else post[key]
-                return jsonify(POSTS[i]), 200
-        return {'error': f'Post with id <{post_id}> not found.'}, 404
+    data = request.get_json()
 
-    elif request.method == 'POST':
-        data = request.get_json()
+    if request.method == 'PUT':
+        update = posts.update(post_id, data)
+        if update:
+            return jsonify(update), 200
 
-        for key, value in data.items():
-            if key not in ['comments', 'categories', 'tags']:
-                return {'error': f'Invalid field: <{key}>'}, 400
+    if request.method == 'POST':
+        attach = posts.attach(post_id, data)
+        if attach:
+            return jsonify(attach), 201
 
-            if key == 'comments':
-                if not isinstance(data.get('comments'), dict):
-                    return {'error': 'Wrong datatype for field: <comments>'}, 400
-
-                if not data['comments'].get('user_id'):
-                    return {'error': 'Missing field: <user>'}, 400
-
-                if not data['comments'].get('comment'):
-                    return {'error': 'Missing field: <comment>'}, 400
-
-            for i, post in enumerate(POSTS):
-                if post['id'] == post_id:
-                    if key not in post:
-                        POSTS[i][key] = []
-
-                    if not key == 'comments' and value in POSTS[i][key]:
-                        return {
-                            'error': f'{key}: <{value}> already exists for post with id <{post_id}>'
-                        }, 400
-
-                    POSTS[i][key].append(data.get(key))
-
-                    return jsonify(POSTS[i]), 201
-    return None
+    return {'error': f'Post with id <{post_id}> not found.'}, 404
 
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
-    title = request.args.get('title', '')
-    content = request.args.get('content', '')
-    search_result = [
-        post for post in POSTS if title.lower() in post['title'].lower() and content.lower() in post['content'].lower()
-    ]
-    return jsonify(search_result), 200
+    posts = JsonHandler()
+    search_result = posts.search(request.args)
+    if search_result:
+        return jsonify(search_result), 200
+    return {'error': 'No posts found.'}, 404
 
 
 if __name__ == '__main__':
--- src/backend/data/json_handler.py
+++ src/backend/data/json_handler.py
@@ -2,6 +2,8 @@ import json
 import os
 from pathlib import Path
 
+from werkzeug.datastructures import MultiDict
+
 
 class JsonHandler:
     __DIR = Path(__file__).parent.resolve()
@@ -12,24 +14,115 @@ class JsonHandler:
     def __init__(self) -> None:
         if not os.path.exists(self.__PATH):
             self._initialize_file()
-        self.__posts = self._load()
-        self.__modified_posts = None
+        self._posts = None
+        self._load()
+        self._result = None
 
     def _initialize_file(self):
         with open(self.__PATH, "w", encoding=self.__ENCODER) as f:
             json.dump([], f)
 
-    def _load(self) -> list[dict[str, str | list[str] | int]]:
+    def _load(self):
         """Loads the JSON file from the predefined path."""
         with open(self.__PATH, "r", encoding=self.__ENCODER) as f:
-            return json.load(f)
+            self._posts = json.load(f)
 
     def _save(self):
         with open(self.__PATH, "w", encoding=self.__ENCODER) as f:
-            json.dump(self.__modified_posts, f)
+            json.dump(self._posts, f, indent=4)
 
-    def get(self):
-        return self.__posts
+    @property
+    def count(self):
+        return len(self._posts)
 
-    def unique_id(self):
-        return max(post['id'] for post in self.__posts) + 1
+    @property
+    def _map(self) -> dict[int, int]:
+        return {self._posts[i]['id']: i for i in range(self.count)}
+
+    def get(self, params: MultiDict[str, str]):
+        x = params.get
+        start = (x('page', 1, type=int) - 1) * x('per_page', self.count, type=int)
+        end = start + x('per_page', self.count, type=int)
+
+        return sorted(
+            self._posts, key=lambda post: post.get(x('sort', 'id')),
+            reverse=(x('direction', 'asc') == 'desc')
+        )[start:end]
+
+    def post(self, post_id):
+        if self._map.get(post_id):
+            return self._posts[self._map.get(post_id)]
+        return None
+
+    @property
+    def _id(self):
+        return max(post['id'] for post in self._posts) + 1
+
+    def add(self, post: dict[str, str | list[str] | int]):
+        new_post = {
+            "id": self._id,
+            "title": post.get('title'),
+            "content": post.get('content'),
+            "comments": [],
+            "categories": [],
+            "tags": []
+        }
+
+        self._posts.append(new_post)
+        self._save()
+        return new_post
+
+    def delete(self, post_id):
+        if self._map.get(post_id):
+            self._posts.pop(self._map.get(post_id))
+            self._save()
+            return {"message": f"Post with id <{post_id}> deleted."}
+        return None
+
+    def update(self, post_id, data):
+        if self._map.get(post_id):
+            self._posts[self._map.get(post_id)].update(data)
+            self._save()
+            return self._posts[self._map.get(post_id)]
+        return None
+
+    def category(self, post_id, category):
+        self._posts[self._map.get(post_id)]['categories'].append(category)
+
+    def comment(self, post_id, comment):
+        self._posts[self._map.get(post_id)]['comments'].append(comment)
+
+    def tag(self, post_id, tag):
+        self._posts[self._map.get(post_id)]['tags'].append(tag)
+
+    def attach(self, post_id, attachments):
+        if self._map.get(post_id):
+            if attachments.get('categories'):
+                self.category(post_id, attachments.get('categories'))
+                self._save()
+
+            if attachments.get('comments'):
+                self.comment(post_id, attachments.get('comments'))
+                self._save()
+
+            if attachments.get('tags'):
+                self.tag(post_id, attachments.get('tags'))
+                self._save()
+
+            self._load()
+            return self._posts[self._map.get(post_id)]
+        return None
+
+    def _init_result(self):
+        self._result = self._posts
+
+    def filter(self, key, value):
+        if value:
+            self._result = [post for post in self._result if str(value).lower() in str(post.get(key)).lower()]
+
+    def search(self, params: MultiDict[str, str]):
+        self._init_result()
+        for param in params:
+            print(self._result)
+            self.filter(param, params.get(param))
+        return self._result
