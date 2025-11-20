e949b6961ef3db2cd48c0e7c6370451093b4e44d
Karim <karim.talbi7@gmail.com>
2025-11-20 17:25:08 +0100

    simplify post management: refactor `JsonHandler` to `Posts`, remove unused fields, optimize methods; drop `flask-login` and `flask-session` dependencies

--- src/backend/backend_app.py
+++ src/backend/backend_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from data.json_handler import JsonHandler
+from data.json_handler import Posts
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
@@ -9,71 +9,49 @@ CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    posts = JsonHandler()
+    posts = Posts()
 
     if request.method == 'POST':
         data = request.get_json()
+        return posts.add(data)
 
-        if not data['title'] or not data['content']:
-            return {'error': f'Missing fields: {[key for key, value in data.items() if not value]}'}, 400
-
-        new_post = posts.add(data)
-        return jsonify(new_post), 201
-
-    return jsonify(posts.get(request.args)), 200
+    return jsonify(posts.posts(request.args)), 200
 
 
-@app.route('/api/posts/<int:post_id>', methods=['GET', 'DELETE', 'PUT', 'POST'])
+@app.route('/api/posts/<int:post_id>', methods=['GET', 'DELETE', 'PUT'])
 def handle_post(post_id):
-    posts = JsonHandler()
+    posts = Posts()
 
     if request.method == 'GET':
-        post = posts.post(post_id)
-        if post:
-            return jsonify(post), 200
+        return posts.post(post_id)
 
     if request.method == 'DELETE':
-        delete = posts.delete(post_id)
-        if delete:
-            return jsonify(delete), 200
-
-    data = request.get_json()
+        return posts.delete(post_id)
 
     if request.method == 'PUT':
-        update = posts.update(post_id, data)
-        if update:
-            return jsonify(update), 200
+        data = request.get_json()
+        return posts.update(post_id, data)
 
-    if request.method == 'POST':
-        attach = posts.attach(post_id, data)
-        if attach:
-            return jsonify(attach), 201
-
-    return {'error': f'Post with id <{post_id}> not found.'}, 404
+    return jsonify({'error': f'Post with id <{post_id}> not found.'}), 404
 
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
-    posts = JsonHandler()
-    search_result = posts.search(request.args)
-    if search_result:
-        return jsonify(search_result), 200
-
-    return {'error': 'No posts found.'}, 404
+    posts = Posts()
+    return posts.search(request.args)
 
 
-SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
-API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/masterblog.json"
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': 'Masterblog API' # (3) You can change this if you like
+        'app_name': 'Masterblog API'
     }
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
--- src/backend/data/json_handler.py
+++ src/backend/data/json_handler.py
@@ -1,128 +1,122 @@
 import json
 import os
 from pathlib import Path
-
 from werkzeug.datastructures import MultiDict
+from datetime import datetime
+from flask import jsonify
 
 
-class JsonHandler:
+class Posts:
     __DIR = Path(__file__).parent.resolve()
-    __NAME = 'posts.json'
-    __PATH = __DIR / __NAME
-    __ENCODER = 'UTF-8'
+    __FILE = 'posts.json'
 
-    def __init__(self) -> None:
-        if not os.path.exists(self.__PATH):
-            self._initialize_file()
-        self._posts = None
-        self._load()
+    def __init__(self):
+        self._path = self.__DIR / self.__FILE
+        self._init()
+        self._data = None
+        if not self._data:
+            self._get()
         self._result = None
 
-    def _initialize_file(self):
-        with open(self.__PATH, "w", encoding=self.__ENCODER) as f:
-            json.dump([], f)
+    def _init(self):
+        if not os.path.exists(self._path):
+            with open(self._path, "w", encoding='UTF-8') as f:
+                json.dump([], f)
 
-    def _load(self):
-        """Loads the JSON file from the predefined path."""
-        with open(self.__PATH, "r", encoding=self.__ENCODER) as f:
-            self._posts = json.load(f)
+    def _get(self):
+        with open(self._path, "r", encoding='UTF-8') as f:
+            self._data = json.load(f)
 
-    def _save(self):
-        with open(self.__PATH, "w", encoding=self.__ENCODER) as f:
-            json.dump(self._posts, f, indent=4)
+    def _set(self):
+        with open(self._path, "w", encoding='UTF-8') as f:
+            json.dump(self._data, f, indent=4)
 
-    @property
-    def count(self):
-        return len(self._posts)
+    def _id(self):
+        if not self._data:
+            return 1
+        return max(post.get('id') for post in self._data) + 1
 
     @property
     def _map(self) -> dict[int, int]:
-        return {self._posts[i]['id']: i for i in range(self.count)}
-
-    def get(self, params: MultiDict[str, str]):
-        x = params.get
-        start = (x('page', 1, type=int) - 1) * x('per_page', self.count, type=int)
-        end = start + x('per_page', self.count, type=int)
-
-        return sorted(
-            self._posts, key=lambda post: post.get(x('sort', 'id')),
-            reverse=(x('direction', 'asc') == 'desc')
-        )[start:end]
+        return {self._data[i].get('id'): i for i in range(len(self._data))}
 
     def post(self, post_id):
-        if self._map.get(post_id):
-            return self._posts[self._map.get(post_id)]
-        return None
+        if self._map.get(post_id) is not None:
+            return jsonify(self._data[self._map.get(post_id)]), 200
+        return {'error': f'Post with id <{post_id}> not found.'}, 404
 
-    @property
-    def _id(self):
-        return max(post['id'] for post in self._posts) + 1
+    def posts(self, params: dict[str, str]):
+        if params.get('sort') == 'date':
+            return sorted(
+                self._data, key=lambda post: datetime.strptime(post.get('date'), "%Y-%m-%d"),
+                reverse=(params.get('direction', 'asc') == 'desc')
+            )
+
+        return sorted(
+            self._data, key=lambda post: post.get(params.get('sort', 'id')),
+            reverse=(params.get('direction', 'asc') == 'desc')
+        )
+
+    @staticmethod
+    def check_unsupported(post: dict[str, str | list[str] | int]):
+        unsupported = []
+        for key in post:
+            if key not in ["title", "content", "author"]:
+                unsupported.append(key)
+        return unsupported
+
+    @staticmethod
+    def check_missing(post: dict[str, str | list[str] | int]):
+        missing = []
+        for key in ["title", "content", "author"]:
+            if key not in post:
+                missing.append(key)
+        return missing
+
 
     def add(self, post: dict[str, str | list[str] | int]):
+        missing = self.check_missing(post)
+        if missing:
+            return jsonify({'error': f'Missing fields: {missing}'}), 400
+
+        unsupported = self.check_unsupported(post)
+        if unsupported:
+            return jsonify({'error': f'Unsupported fields: {unsupported}'}), 400
+
         new_post = {
-            "id": self._id,
-            "title": post.get('title'),
-            "content": post.get('content'),
-            "comments": [],
-            "categories": [],
-            "tags": []
+            "id": self._id(),
+            "date": datetime.now().strftime("%Y-%m-%d")
         }
 
-        self._posts.append(new_post)
-        self._save()
-        return new_post
+        new_post.update(post)
+
+        self._data.append(new_post)
+        self._set()
+        return jsonify(new_post), 201
 
     def delete(self, post_id):
-        if self._map.get(post_id):
-            self._posts.pop(self._map.get(post_id))
-            self._save()
-            return {"message": f"Post with id <{post_id}> deleted."}
-        return None
+        if self._map.get(post_id) is not None:
+            self._data.pop(self._map.get(post_id))
+            self._set()
+            return jsonify({"message": f"Post with id <{post_id}> deleted."}), 200
+        return {'error': f'Post with id <{post_id}> not found.'}, 404
 
     def update(self, post_id, data):
-        if self._map.get(post_id):
-            self._posts[self._map.get(post_id)].update(data)
-            self._save()
-            return self._posts[self._map.get(post_id)]
-        return None
-
-    def category(self, post_id, category):
-        self._posts[self._map.get(post_id)]['categories'].append(category)
-
-    def comment(self, post_id, comment):
-        self._posts[self._map.get(post_id)]['comments'].append(comment)
-
-    def tag(self, post_id, tag):
-        self._posts[self._map.get(post_id)]['tags'].append(tag)
-
-    def attach(self, post_id, attachments):
-        if self._map.get(post_id):
-            if attachments.get('categories'):
-                self.category(post_id, attachments.get('categories'))
-                self._save()
-
-            if attachments.get('comments'):
-                self.comment(post_id, attachments.get('comments'))
-                self._save()
-
-            if attachments.get('tags'):
-                self.tag(post_id, attachments.get('tags'))
-                self._save()
-
-            self._load()
-            return self._posts[self._map.get(post_id)]
-        return None
-
-    def _init_result(self):
-        self._result = self._posts
+        if self._map.get(post_id) is not None:
+            self._data[self._map.get(post_id)].update(data)
+            self._set()
+            return jsonify(self._data[self._map.get(post_id)]), 200
+        return {'error': f'Post with id <{post_id}> not found.'}, 404
 
     def filter(self, key, value):
         if value:
             self._result = [post for post in self._result if str(value).lower() in str(post.get(key)).lower()]
 
     def search(self, params: MultiDict[str, str]):
-        self._init_result()
+        self._result = self._data.copy()
         for param in params:
             print(self._result)
             self.filter(param, params.get(param))
-        return self._result
+            if not self._result:
+                return {'error': 'No posts found.'}, 404
+        return jsonify(self._result), 200
